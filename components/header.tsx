527e2f5e047ffefa26f5be6c5a32f44e0104e504
coreyfmiller <coreyfmiller@gmail.com>
2026-05-27 15:27:54 -0300

    Use correct Logo.png from public root

--- components/header.tsx
+++ components/header.tsx
@@ -21,7 +21,7 @@ export function Header() {
       <div className="container mx-auto flex h-24 items-center justify-between px-4 md:px-6">
         <Link href="/" className="flex items-center gap-2">
           <Image
-            src="/images/logo.png"
+            src="/Logo.png"
             alt="KV Launch"
             width={280}
             height={80}
